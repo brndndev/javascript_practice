cecfbacf0fcfa2b683fee61934fd314e7fc4b32a
Brndndev <brndndev@outlook.com>
2021-11-17 18:01:04 -0500

    👍🏾 all good

---
+++ Notebook/test.py
@@ -0,0 +1,18 @@
+
+# COMMENT OUT: command + /
+
+#  create a function that tells me how old I am
+#  then guess my birthday
+#  if it is correct, say "Correct!"
+
+import DateTime
+
+name = "Brandon"
+age = "29"
+birthday = "01/06/1992
+
+# new DateTime(2009, 02, 27).ToString("MMMM dd, yyyy") // February 27, 2009"
+
+def getmyage():
+
+    new DateTime()
--- README.md
+++
@@ -1,4 +0,0 @@
-# Resume_CV_Design
-# brandonprice_resume_csv
-brandonprice_resume_csv
-# javascript_practice
