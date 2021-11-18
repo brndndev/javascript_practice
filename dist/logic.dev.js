937bcfbaa1752c90914a35faa1ead313d0c29e2b
Brndndev <brndndev@outlook.com>
2021-11-18 15:27:25 -0500

    👓logic.js added and tested if, else if, and else

---
+++ dist/logic.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var age = 21;
+
+if (age < 21) {
+  console.log("Kick rocks, kid! 🖕🏾");
+} else age >= 21;
+
+{
+  console.log("Welcome to the party , bro 🗣");
+}
---
+++ logic.js
@@ -0,0 +1,8 @@
+let age = 21
+
+if (age < 21) {
+    console.log("Kick rocks, kid! 🖕🏾")
+} else (age >= 21); {
+    console.log("Welcome to the party , bro 🗣")
+
+}
